c91474cd73d941b06cadc3919e9d5dc539cd80ff
Wowkster <adyowowk@gmail.com>
2021-12-05 18:14:52 -0500

    Typescript Support

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,39 @@
-const gulp = require('gulp');
-const jasmine = require('gulp-jasmine');
-const gutil = require('gulp-util');
+const { src, task, watch } = require('gulp')
+const jasmine = require('gulp-jasmine')
+const { log, colors } = require('gulp-util')
 
-const srcFiles = 'resemble.js';
-const specFiles = 'resemble.spec.js';
+const srcFiles = './dist/index.js'
+const specFiles = './index.spec.js'
 
 function runTests(breakOnError) {
-  return gulp.src(specFiles)
-    .pipe(jasmine({
-      includeStackTrace: true
-    }))
-    .on('error', errorHandler(breakOnError));
+    return src(specFiles)
+        .pipe(
+            jasmine({
+                includeStackTrace: true,
+            })
+        )
+        .on('error', errorHandler(breakOnError))
 }
 
 function errorHandler(breakOnError) {
-  return function(error) {
-    if (breakOnError) {
-      throw error;
-    } else {
-      gutil.log(gutil.colors.red('[Jasmine]'), error.toString());
-      this.emit('end');
+    return function (error) {
+        if (breakOnError) {
+            throw error
+        } else {
+            log(colors.red('[Jasmine]'), error.toString())
+            this.emit('end')
+        }
     }
-  }
 }
 
-gulp.task('test', function() {
-  return runTests(true);
-});
+task('test', function () {
+    return runTests(true)
+})
 
-gulp.task('test:auto', function() {
-  runTests(false);
+task('test:auto', function () {
+    runTests(false)
 
-  return gulp.watch([srcFiles, specFiles], function() {
-    runTests(false);
-  });
-});
+    return watch([srcFiles, specFiles], function () {
+        runTests(false)
+    })
+})
